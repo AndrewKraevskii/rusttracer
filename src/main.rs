e0346e526422ce3ddddaf7953c90e8100502b9ad
andrewkraevskii <andrew.kraevskii@gmail.com>
2024-03-08 01:36:28 +0300

    Split run function into two
    At least it is not as bad as before. But improvments still there

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use encase::ShaderType;
 use tracing::debug;
 use wgpu::{
-    include_wgsl, BufferDescriptor, BufferUsages, Color, CommandEncoderDescriptor,
-    DeviceDescriptor, FragmentState, PipelineLayoutDescriptor, PowerPreference,
-    RenderPassColorAttachment, RenderPassDescriptor, RenderPipelineDescriptor,
-    TextureViewDescriptor, VertexState,
+    include_wgsl, BindGroupLayoutEntry, BufferDescriptor, BufferUsages, Color,
+    CommandEncoderDescriptor, DeviceDescriptor, FragmentState, PipelineLayoutDescriptor,
+    PowerPreference, RenderPassColorAttachment, RenderPassDescriptor, RenderPipelineDescriptor,
+    ShaderStages, Surface, SurfaceConfiguration, TextureViewDescriptor, VertexState,
 };
 use winit::{
+    error::EventLoopError,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::Window,
@@ -19,134 +24,261 @@ async fn main() -> anyhow::Result<()> {
     event_loop.set_control_flow(ControlFlow::Wait);
     let window = Window::new(&event_loop).unwrap();
 
-    run(event_loop, window).await?;
+    App::init(window).await?.run(event_loop).await?;
     Ok(())
 }
 
-async fn run(event_loop: EventLoop<()>, window: Window) -> anyhow::Result<()> {
-    let size = window.inner_size();
-    let width = size.width;
-    let height = size.height;
+#[derive(Debug, ShaderType)]
+struct UniformState {
+    time: f32,
+    apsect: f32, // w/h
+}
 
-    let instance = wgpu::Instance::default();
-    debug!("Got instance");
-    let surface = instance.create_surface(&window)?;
-    debug!("Created surface");
+struct App {
+    surface: Surface<'static>,
+    window: Arc<Window>,
+    config: SurfaceConfiguration,
+    state: UniformState,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    uniform_buffer: wgpu::Buffer,
+    bind_group: wgpu::BindGroup,
+    pipeline: wgpu::RenderPipeline,
+}
 
-    let Some(adapter) = instance
-        .request_adapter(&wgpu::RequestAdapterOptions {
-            compatible_surface: Some(&surface),
-            power_preference: PowerPreference::HighPerformance,
-            force_fallback_adapter: false,
-        })
-        .await
-    else {
-        return Err(anyhow::anyhow!("Can't get adapter"));
-    };
-    debug!("Got adapter");
+impl App {
+    async fn run(mut self: Self, event_loop: EventLoop<()>) -> Result<(), EventLoopError> {
+        event_loop.run(move |event, elwt| {
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => elwt.exit(),
+                Event::WindowEvent {
+                    event: WindowEvent::Resized(size),
+                    ..
+                } => {
+                    self.config.width = size.width;
+                    self.config.height = size.height;
 
-    let (device, queue) = adapter
-        .request_device(
-            &DeviceDescriptor {
-                label: Some("Device"),
-                required_features: adapter.features(),
-                required_limits: wgpu::Limits::downlevel_webgl2_defaults()
-                    .using_resolution(adapter.limits()),
-            },
-            None,
-        )
-        .await?;
-
-    debug!("Got device");
-    let _buffer = device.create_buffer(&BufferDescriptor {
-        label: None,
-        size: 10,
-        usage: BufferUsages::COPY_DST | BufferUsages::UNIFORM,
-        mapped_at_creation: false,
-    });
-
-    let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
-
-    let render_pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
-        label: None,
-        bind_group_layouts: &[],
-        push_constant_ranges: &[],
-    });
-    let swapchain_capabilities = surface.get_capabilities(&adapter);
-    let swapchain_format = swapchain_capabilities.formats[0];
-
-    let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
-        label: None,
-        layout: Some(&render_pipeline_layout),
-        vertex: VertexState {
-            module: &shader,
-            entry_point: "vs_main",
-            buffers: &[],
-        },
-        primitive: Default::default(),
-        depth_stencil: Default::default(),
-        multisample: Default::default(),
-        fragment: Some(FragmentState {
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[Some(swapchain_format.into())],
-        }),
-        multiview: Default::default(),
-    });
-
-    let mut config = surface.get_default_config(&adapter, width, height).unwrap();
-    surface.configure(&device, &config);
-
-    let window = &window;
-    event_loop.run(move |event, elwt| {
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => elwt.exit(),
-            Event::WindowEvent {
-                event: WindowEvent::Resized(size),
-                ..
-            } => {
-                config.width = size.width;
-                config.height = size.height;
-
-                surface.configure(&device, &config);
-                window.request_redraw();
-            }
-            Event::WindowEvent {
-                event: WindowEvent::RedrawRequested,
-                ..
-            } => {
-                let surface_texture = surface.get_current_texture().expect("To get texture");
-                let mut encoder =
-                    device.create_command_encoder(&CommandEncoderDescriptor::default());
-                let view = surface_texture
-                    .texture
-                    .create_view(&TextureViewDescriptor::default());
-                {
-                    let mut pass = encoder.begin_render_pass(&RenderPassDescriptor {
-                        label: None,
-                        color_attachments: &[Some(RenderPassColorAttachment {
-                            view: &view,
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(Color::GREEN),
-                                store: wgpu::StoreOp::Store,
-                            },
-                        })],
-                        depth_stencil_attachment: None,
-                        timestamp_writes: None,
-                        occlusion_query_set: None,
-                    });
-                    pass.set_pipeline(&pipeline);
-                    pass.draw(0..3, 0..1);
+                    self.state.apsect = self.config.width as f32 / self.config.height as f32;
+                    self.surface.configure(&self.device, &self.config);
+                    self.window.request_redraw();
                 }
-                queue.submit(Some(encoder.finish()));
-                surface_texture.present();
-            }
-            _ => (),
+                Event::WindowEvent {
+                    event: WindowEvent::RedrawRequested,
+                    ..
+                } => {
+                    self.window.request_redraw();
+                    let surface_texture =
+                        self.surface.get_current_texture().expect("To get texture");
+                    let mut encoder = self
+                        .device
+                        .create_command_encoder(&CommandEncoderDescriptor::default());
+                    let view = surface_texture
+                        .texture
+                        .create_view(&TextureViewDescriptor::default());
+
+                    // self.state.time = std::time::Instant::now()
+                    //     .duration_since(self.start_time)
+                    // .as_secs_f32();
+                    self.state.time = 0.0;
+                    self.queue.write_buffer(
+                        &self.uniform_buffer,
+                        0,
+                        &self.state.as_wgsl_bytes().expect(
+                            "Error in encase translating AppState \
+                    struct to WGSL bytes.",
+                        ),
+                    );
+                    {
+                        let mut pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                            label: None,
+                            color_attachments: &[Some(RenderPassColorAttachment {
+                                view: &view,
+                                resolve_target: None,
+                                ops: wgpu::Operations {
+                                    load: wgpu::LoadOp::Clear(Color::BLACK),
+                                    store: wgpu::StoreOp::Store,
+                                },
+                            })],
+                            depth_stencil_attachment: None,
+                            timestamp_writes: None,
+                            occlusion_query_set: None,
+                        });
+                        pass.set_pipeline(&self.pipeline);
+                        pass.set_bind_group(0, &self.bind_group, &[]);
+                        pass.draw(0..6, 0..1);
+                    }
+                    self.queue.submit(Some(encoder.finish()));
+                    surface_texture.present();
+                }
+                _ => (),
+            };
+        })
+    }
+
+    async fn init(window: Window) -> anyhow::Result<Self> {
+        let window = Arc::new(window);
+        // let start_time = std::time::Instant::now();
+
+        let size = window.inner_size();
+        let width = size.width;
+        let height = size.height;
+
+        let instance = wgpu::Instance::default();
+        debug!("Got instance");
+        let surface = instance.create_surface(window.clone())?;
+        debug!("Created surface");
+
+        let Some(adapter) = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                compatible_surface: Some(&surface),
+                power_preference: PowerPreference::HighPerformance,
+                force_fallback_adapter: false,
+            })
+            .await
+        else {
+            return Err(anyhow::anyhow!("Can't get adapter"));
         };
-    })?;
-    Ok(())
+        debug!("Got adapter");
+
+        let (device, queue) = adapter
+            .request_device(
+                &DeviceDescriptor {
+                    label: Some("Device"),
+                    required_features: adapter.features(),
+                    required_limits: wgpu::Limits::downlevel_webgl2_defaults()
+                        .using_resolution(adapter.limits()),
+                },
+                None,
+            )
+            .await?;
+
+        debug!("Got device");
+        let _buffer = device.create_buffer(&BufferDescriptor {
+            label: None,
+            size: 10,
+            usage: BufferUsages::COPY_DST | BufferUsages::UNIFORM,
+            mapped_at_creation: false,
+        });
+
+        let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
+
+        let uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: std::mem::size_of::<UniformState>() as u64,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: None,
+            entries: &[BindGroupLayoutEntry {
+                binding: 0,
+                visibility: ShaderStages::FRAGMENT | ShaderStages::VERTEX,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                    buffer: &uniform_buffer,
+                    offset: 0,
+                    size: None,
+                }),
+            }],
+        });
+        let render_pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &[&bind_group_layout],
+            push_constant_ranges: &[],
+        });
+        let swapchain_capabilities = surface.get_capabilities(&adapter);
+        let swapchain_format = swapchain_capabilities.formats[0];
+
+        let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&render_pipeline_layout),
+            vertex: VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            primitive: Default::default(),
+            depth_stencil: Default::default(),
+            multisample: Default::default(),
+            fragment: Some(FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(swapchain_format.into())],
+            }),
+            multiview: Default::default(),
+        });
+
+        let config = surface.get_default_config(&adapter, width, height).unwrap();
+        surface.configure(&device, &config);
+
+        let state = UniformState {
+            time: 0.0,
+            apsect: width as f32 / height as f32,
+        };
+
+        Ok(Self {
+            state,
+            surface,
+            device,
+            queue,
+            config,
+            window,
+            uniform_buffer,
+            bind_group,
+            pipeline,
+        })
+    }
+}
+
+impl UniformState {
+    // Translating Rust structures to WGSL is always tricky and can prove
+    // incredibly difficult to remember all the rules by which WGSL
+    // lays out and formats structs in memory. It is also often extremely
+    // frustrating to debug when things don't go right.
+    //
+    // You may sometimes see structs translated to bytes through
+    // using `#[repr(C)]` on the struct so that the struct has a defined,
+    // guaranteed internal layout and then implementing bytemuck's POD
+    // trait so that one can preform a bitwise cast. There are issues with
+    // this approach though as C's struct layouts aren't always compatible
+    // with WGSL, such as when special WGSL types like vec's and mat's
+    // get involved that have special alignment rules and especially
+    // when the target buffer is going to be used in the uniform memory
+    // space.
+    //
+    // Here though, we use the encase crate which makes translating potentially
+    // complex Rust structs easy through combined use of the [`ShaderType`] trait
+    // / derive macro and the buffer structs which hold data formatted for WGSL
+    // in either the storage or uniform spaces.
+    fn as_wgsl_bytes(&self) -> encase::internal::Result<Vec<u8>> {
+        let mut buffer = encase::UniformBuffer::new(Vec::new());
+        buffer.write(self)?;
+        Ok(buffer.into_inner())
+    }
+
+    fn translate_view(&mut self, increments: i32, axis: usize) {
+        // self.cursor_pos[axis] += CAMERA_POS_INCREMENT_FACTOR * increments as f32 / self.zoom;
+    }
+
+    fn zoom(&mut self, amount: f32) {
+        // self.zoom += ZOOM_INCREMENT_FACTOR * amount * self.zoom.powf(1.02);
+        // self.zoom = self.zoom.max(1.1);
+    }
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,12 +1,152 @@
-@vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let x = f32(i32(in_vertex_index) - 1);
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-    return vec4<f32>(x, y, 0.0, 1.0);
+struct VertexOutput {
+    @location(0) position: vec4<f32>,
+    @builtin(position) screen_coord: vec4<f32>,
+};
+
+
+struct Sphere {
+    center: vec3f,
+    radius: f32,
+};
+
+struct Ray {
+    start: vec3f,
+    direction: vec3f,
+};
+
+struct IntersectionPoint {
+    distance: f32,
+    point: vec3f,
+    normal: vec3f,
+};
+struct PerspectiveCamera {
+    position: vec3f,
+    right: vec3f,
+    up: vec3f,
+    height: f32,
+    width: f32,
+    focal_distance: f32,
 }
 
+const camera = PerspectiveCamera(
+    vec3f(0, 0, 20),
+    vec3f(25, 0, 0),
+    vec3f(0, 25, 0),
+    320,
+    320,
+    50,
+);
+
+fn top_left() -> vec3f {
+    return camera.position - normalize(camera.right) * camera.width +  normalize(camera.up) * camera.height; 
+}
+fn top_right() -> vec3f {
+    return camera.position + normalize(camera.right) * camera.width +  normalize(camera.up) * camera.height; 
+}
+fn bottom_left() -> vec3f {
+    return camera.position - normalize(camera.right) * camera.width -  normalize(camera.up) * camera.height; 
+}
+fn bottom_right() -> vec3f {
+    return camera.position + normalize(camera.right) * camera.width -  normalize(camera.up) * camera.height; 
+}
+fn front() -> vec3f {
+    return cross(camera.right, camera.up);
+}
+fn get_ray(x: f32, y: f32) -> Ray {
+    let start = mix(
+        mix(top_left(), top_right(), x),
+        mix(bottom_left(), bottom_right(), x),
+        y,
+    );
+    return Ray(
+        start,
+        start - (camera.position -
+                normalize(front()) *
+                camera.focal_distance)
+    );
+}
+
+
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> VertexOutput {
+    var vertices = array<vec2<f32>, 6>(
+        vec2<f32>(-1, -1),
+        vec2<f32>(-1, 1),
+        vec2<f32>(1, 1),
+        vec2<f32>(1, 1),
+        vec2<f32>(1, -1),
+        vec2<f32>(-1, -1),
+    );
+    
+    var result: VertexOutput;
+    result.position = vec4<f32>(vertices[in_vertex_index], 0.0, 1.0);
+    result.screen_coord = result.position ;
+    return result;
+}
+
+fn sq(v: f32) -> f32 {
+    return v * v;
+}
+
+fn intersectRaySphere(ray: Ray, sphere: Sphere) -> array<IntersectionPoint, 2> {
+    let discriminant: f32 = sq(dot(ray.direction, ray.start - sphere.center)) +
+        dot(ray.direction, ray.direction) * (sq(sphere.radius) - dot(ray.start, ray.start) +
+        2 * dot(ray.start, sphere.center) - dot(sphere.center, sphere.center));
+    if (discriminant < 0) {
+        discard;
+    }
+    let res: f32 = dot(ray.direction, (sphere.center - ray.start)) / dot(ray.direction, ray.direction);
+    let distance = array<f32, 2>(
+        res - sqrt(discriminant) / dot(ray.direction, ray.direction),
+        res + sqrt(discriminant) / dot(ray.direction, ray.direction),
+    );
+
+    let normals = array<vec3f, 2>(
+        normalize(ray.start + ray.direction * distance[0] - sphere.center),
+        normalize(ray.start + ray.direction * distance[1] - sphere.center),
+    );
+
+    return array<IntersectionPoint, 2>(
+            IntersectionPoint(
+               distance[0],
+               normals[0],
+             ray.start + ray.direction * distance[0],
+            ),
+            IntersectionPoint(
+               distance[1],
+               normals[1],
+             ray.start + ray.direction * distance[1],
+            )
+        );
+}
+
+struct UniformState {
+    time: f32,
+    apsect: f32, // w/h
+}
+
+@group(0)
+@binding(0)
+var<uniform> state: UniformState;
+
+const sphere = Sphere(
+    vec3f(0, 0, 0),
+    20
+);
+
 @fragment
-fn fs_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+fn fs_main(
+    vertex: VertexOutput,
+) -> @location(0) vec4<f32> {
+    let pos_x = (vertex.position.x + 1) / 2;
+    let pos_y = (vertex.position.y + 1) / 2;
+
+    let ray = get_ray(pos_x, pos_y + sin(state.time) / 2);
+    let intersect = intersectRaySphere(ray, sphere);
+
+    let light = normalize(vec3f(sin(state.time), cos(state.time), 1));
+
+    
+    return vec4<f32>(max(vec2f(dot(-light, normalize(intersect[0].normal))), vec2f(0.2)), sin(state.time), 1.0);
 }
 
